888d75ca6f3a9081a1f2f0af1368ad521566c630
rinrinne <rinrin.ne@gmail.com>
2014-09-08 11:01:23 +0900

    Replace words for rbenv
    s/rbenv/ndkenv/g
    s/RBENV/NDKENV/g
    s/ruby/ndk/g
    s/Ruby/NDK/g
    s/RUBY/NDK/g

--- completions/ndkenv.bash
+++ completions/ndkenv.bash
@@ -1,16 +1,16 @@
-_rbenv() {
+_ndkenv() {
   COMPREPLY=()
   local word="${COMP_WORDS[COMP_CWORD]}"
 
   if [ "$COMP_CWORD" -eq 1 ]; then
-    COMPREPLY=( $(compgen -W "$(rbenv commands)" -- "$word") )
+    COMPREPLY=( $(compgen -W "$(ndkenv commands)" -- "$word") )
   else
     local words=("${COMP_WORDS[@]}")
     unset words[0]
     unset words[$COMP_CWORD]
-    local completions=$(rbenv completions "${words[@]}")
+    local completions=$(ndkenv completions "${words[@]}")
     COMPREPLY=( $(compgen -W "$completions" -- "$word") )
   fi
 }
 
-complete -F _rbenv rbenv
+complete -F _ndkenv ndkenv
--- completions/ndkenv.fish
+++ completions/ndkenv.fish
@@ -1,12 +1,12 @@
-function __fish_rbenv_needs_command
+function __fish_ndkenv_needs_command
   set cmd (commandline -opc)
-  if [ (count $cmd) -eq 1 -a $cmd[1] = 'rbenv' ]
+  if [ (count $cmd) -eq 1 -a $cmd[1] = 'ndkenv' ]
     return 0
   end
   return 1
 end
 
-function __fish_rbenv_using_command
+function __fish_ndkenv_using_command
   set cmd (commandline -opc)
   if [ (count $cmd) -gt 1 ]
     if [ $argv[1] = $cmd[2] ]
@@ -16,7 +16,7 @@ function __fish_rbenv_using_command
   return 1
 end
 
-complete -f -c rbenv -n '__fish_rbenv_needs_command' -a '(rbenv commands)'
-for cmd in (rbenv commands)
-  complete -f -c rbenv -n "__fish_rbenv_using_command $cmd" -a "(rbenv completions $cmd)"
+complete -f -c ndkenv -n '__fish_ndkenv_needs_command' -a '(ndkenv commands)'
+for cmd in (ndkenv commands)
+  complete -f -c ndkenv -n "__fish_ndkenv_using_command $cmd" -a "(ndkenv completions $cmd)"
 end
